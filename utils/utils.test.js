275fa3af289807bbf5aecac2a025d79877a87998
ReadyDoMe <unitecomrades777@gmail.com>
2019-02-13 22:56:31 -0600

    installed assertion lib and made some tests

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,16 +1,21 @@
+const expect = require('expect');
+
 const utils = require('./utils');
 
 it('should add two numbers', () => {
   var res = utils.add(1, 2);
-  
-  if (res !== 3) {
-    throw new Error(`Expected 3 but got ${res}`);
-  }
+  expect(res).toBe(3).toBeA('number');
 });
 
 it('should square a number', () => {
   var res = utils.square(5);
-  if (res !== 25) {
-    throw new Error(`Expected 25 but got ${res}`);
-  }
+  expect(res).toBe(25).toBeA('number');
+});
+
+it('should verify first and last names are set', () => {
+  var res = utils.setName({age: 28, location: 'Austin'}, 'Nick Piazza');
+  expect(res).toInclude({
+    firstName: 'Nick',
+    lastName: 'Piazza',
+  });
 });
